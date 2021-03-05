d1c639029250b61ed12e34d8dc8fe6d9d15c6499
Tsarcasm <lockiecharles@gmail.com>
2021-03-05 00:25:33 +0000

    Add project files.

---
+++ STMSerialMonitor/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace STMSerialMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.httpbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Com port:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "one",
+            "two",
+            "three"});
+            this.listBox1.Location = new System.Drawing.Point(12, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(137, 25);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1217, 498);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // httpbox
+            // 
+            this.httpbox.Location = new System.Drawing.Point(12, 172);
+            this.httpbox.Multiline = true;
+            this.httpbox.Name = "httpbox";
+            this.httpbox.ReadOnly = true;
+            this.httpbox.Size = new System.Drawing.Size(119, 351);
+            this.httpbox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1366, 535);
+            this.Controls.Add(this.httpbox);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox httpbox;
+    }
+}
+
---
+++ STMSerialMonitor/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace STMSerialMonitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = flowLayoutPanel1.Controls.Count - 1; i >= 0; i--)
+            {
+                (SerialPort port, bool paused) = ((SerialPort, bool))flowLayoutPanel1.Controls[i].Tag;
+                port.Close();
+                flowLayoutPanel1.Controls.RemoveAt(i);
+            }
+        }
+
+        string previousPorts = "";
+        SerialPort port;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Get a list of serial port names.
+            string[] ports = SerialPort.GetPortNames();
+
+            // If we have a change in ports then represent that
+            if (previousPorts != ports.Aggregate((i, j) => i + j))
+            {
+                previousPorts = ports.Aggregate((i, j) => i + j);
+                foreach (string portString in ports)
+                {
+                    if (portString == "COM1") continue;
+                    bool found = false;
+                    // See if we already have a panel for this port
+                    foreach (Panel item in flowLayoutPanel1.Controls)
+                    {
+                        (SerialPort port, bool paused) = ((SerialPort, bool))item.Tag;
+                        if (port.PortName == portString) found = true;
+                    }
+
+                    //If we don't have a panel then add one 
+                    if (!found)
+                    {
+                        var newPort = new SerialPort(portString, 115200);
+                        newPort.Open();
+                        addPortBox(newPort);
+                    }
+                }
+
+            }
+
+            for (int i = flowLayoutPanel1.Controls.Count - 1; i >= 0; i--)
+            {
+                Panel p = (Panel)flowLayoutPanel1.Controls[i];
+                TextBox textBox = (TextBox)p.Controls[0];
+                (SerialPort port, bool paused) = ((SerialPort, bool))p.Tag;
+                lock (port)
+                {
+                    if (!port.IsOpen && !paused)
+                    {
+                        try
+                        {
+                            textBox.Enabled = false;
+                            if (ports.Contains(port.PortName))
+                            {
+                                port.Open();
+                                if (port.IsOpen)
+                                {
+                                    textBox.Enabled = true;
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Error");
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+                    }
+                    else
+                    {
+                        if (port.IsOpen)
+                        {
+                            if (((CheckBox)p.Controls[4]).Checked)
+                            {
+                                string portStr = port.ReadExisting();
+                                if (portStr.Contains("DATA_UPLOAD"))
+                                {
+                                    upload(portStr);
+                                }
+
+                                ((TextBox)p.Controls[0]).AppendText(portStr);
+                            }
+                        }
+                    }
+                }
+            }
+
+
+
+
+        }
+
+        static WebClient webClient = new WebClient();
+        private void upload(string str)
+        {
+            // First get a token
+            string result = webClient.DownloadString("http://157.245.47.19/moonitoring/api/start_upload.php?b=1&f=2");
+            int token = int.Parse(JsonConvert.DeserializeObject<Dictionary<string, string>>(result)["token"]);
+            var data = str.Split('|')[1];
+            httpbox.AppendText($"Obtained token {token}\n");
+            var uploadStr = $"t={token}&{data}";
+            httpbox.AppendText($"Data request: {uploadStr}\n");
+            result = webClient.DownloadString("http://157.245.47.19/moonitoring/api/upload_data.php?" + uploadStr);
+            httpbox.AppendText(result);
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string newPort = (string)listBox1.SelectedItem;
+            if (newPort == null)
+            {
+                return;
+            }
+            port = new SerialPort(newPort, 115200);
+            port.Open();
+            //textBox1.Enabled = true;
+        }
+
+
+        void addPortBox(SerialPort port)
+        {
+            Panel panel = new Panel()
+            {
+                Width = 400,
+                Height = flowLayoutPanel1.Height,
+                BorderStyle = BorderStyle.FixedSingle,
+                Tag = (port, false),
+            };
+
+            Label label = new Label()
+            {
+                Text = port.PortName,
+                Location = new Point(3, 3),
+            };
+
+
+            TextBox textBox = new TextBox()
+            {
+                Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right,
+                Location = new Point(3, 35),
+                Multiline = true,
+                ScrollBars = ScrollBars.Vertical,
+                Size = new Size(400, flowLayoutPanel1.Height - 80),
+                TabIndex = 0,
+                Font = new Font("Consolas", 10)
+            };
+
+            Button clearBtn = new Button()
+            {
+                Text = "Clear",
+                Top = flowLayoutPanel1.Height - 30,
+                Size = new Size(100, 25)
+            };
+            clearBtn.Click += (_, __) =>
+            {
+                textBox.Clear();
+            };
+            Button pauseBtn = new Button()
+            {
+                Text = "Pause",
+                Top = flowLayoutPanel1.Height - 30,
+                Left = 110,
+                Size = new Size(100, 25)
+            };
+
+            pauseBtn.Click += (_, __) =>
+            {
+                (SerialPort _p, bool paused) = ((SerialPort, bool))panel.Tag;
+                lock (_p)
+                {
+                    if (paused)
+                    {
+                        pauseBtn.Text = "Pause";
+                        panel.Tag = (_p, false);
+                    }
+                    else
+                    {
+                        pauseBtn.Text = "Unpause";
+                        _p.Close();
+                        panel.Tag = (_p, true);
+                        textBox.Enabled = false;
+                    }
+                }
+            };
+
+            CheckBox checkBox = new CheckBox()
+            {
+                Text = "Auto Print",
+                Checked = true,
+                Top = flowLayoutPanel1.Height - 30,
+                Left = 220
+            };
+
+            panel.Controls.Add(textBox);
+            panel.Controls.Add(label);
+            panel.Controls.Add(clearBtn);
+            panel.Controls.Add(pauseBtn);
+            panel.Controls.Add(checkBox);
+            flowLayoutPanel1.Controls.Add(panel);
+
+        }
+    }
+}
